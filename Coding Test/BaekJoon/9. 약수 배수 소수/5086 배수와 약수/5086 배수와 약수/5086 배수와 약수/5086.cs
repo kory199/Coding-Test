cb8dacfda98e4a0bf45f04984954f7b213348e9c
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-05-22 13:58:20 +0900

    백준 배수와 약수 소수
    5086 배수와 약수 업로드

---
+++ Coding Test/BaekJoon/9. 약수 배수 소수/5086 배수와 약수/5086 배수와 약수/5086 배수와 약수/5086.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _5086_배수와_약수
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int factor = 0;
+            int multiple = 0;
+            int neither = 0;
+            int temp1 = 0;
+            int temp2 = 0;
+
+
+            while (true)
+            {
+                factor = 0;
+                multiple = 0;
+                neither = 0;
+                temp1 = 0;
+                temp2 = 0;
+
+                string[] inputStr = Console.ReadLine().Split(' ');
+                num1 = int.Parse(inputStr[0]);
+                num2 = int.Parse(inputStr[1]);
+
+                if (num1 == 0 && num2 == 0)
+                {
+                    break;
+                }
+
+                factor = num2 / num1;
+                temp1 = num2 % num1;
+                multiple = num1 / num2;
+                temp2 = num1 % num2;
+
+                if (factor > 0 && temp1 == 0)
+                {
+                    Console.WriteLine("factor");
+                }
+
+                else if (multiple > 0 && temp2 == 0)
+                {
+                    Console.WriteLine("multiple");
+                }
+
+                else
+                {
+                    Console.WriteLine("neither");
+                }
+            }
+        }
+    }
+}
