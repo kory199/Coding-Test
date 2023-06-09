07e35174f38bd607616d05caa986b4f68583acf4
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-06-09 16:48:13 +0900

    백준_알고리즘수업2

--- Coding Test/BaekJoon/시간 복잡도/시간복잡도/시간복잡도/알고리즘수업_알고리즘의수행시간2.cs
+++ Coding Test/BaekJoon/시간 복잡도/시간복잡도/시간복잡도/알고리즘수업_알고리즘의수행시간2.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace 시간복잡도
 {
-    class 알고리즘수업_알고리즘의수행시간1
+    class 알고리즘수업_알고리즘의수행시간2
     {
         public void solution()
         {
             int n;
             n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n);
             Console.WriteLine(1);
-            Console.WriteLine(0);
         }
     }
 }
