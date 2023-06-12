5e6f172fe745127a061acc99de023e99148f0888
kory199 <97496901+kory199@users.noreply.github.com>
2023-06-12 14:34:39 +0900

    백준 2581 소수

--- Coding Test/BaekJoon/9. 약수 배수 소수/5086 배수와 약수/5086 배수와 약수/5086 배수와 약수/2581소수.cs
+++ Coding Test/BaekJoon/9. 약수 배수 소수/5086 배수와 약수/5086 배수와 약수/5086 배수와 약수/2581소수.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace _5086_배수와_약수
@@ -8,7 +9,35 @@ namespace _5086_배수와_약수
     {
         public static void Solution()
         {
+            List<int> decimalNumbers = new List<int>();
+            int Num1 = int.Parse(Console.ReadLine());
+            int Num2 = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i <= Num2 - Num1; i++)
+            {
+                int temp = 0;
+                int transNum = i + Num1;
+
+                if (Num1 == 0)
+                    continue;
+
+                for (int j = 1; j <= transNum; j++)
+                {
+                    if (transNum % j == 0)
+                        temp += j;
+                }
+
+                if (temp == transNum + 1)
+                    decimalNumbers.Add(transNum);
+            }
+
+            if (decimalNumbers.Count == 0)
+                Console.WriteLine(-1);
+            else
+            {
+                Console.WriteLine(decimalNumbers.Sum());
+                Console.WriteLine(decimalNumbers[0]);
+            }
         }
     }
 }
--- Coding Test/BaekJoon/9. 약수 배수 소수/5086 배수와 약수/5086 배수와 약수/5086 배수와 약수/Main.cs
+++ Coding Test/BaekJoon/9. 약수 배수 소수/5086 배수와 약수/5086 배수와 약수/5086 배수와 약수/Main.cs
@@ -6,7 +6,7 @@ namespace _5086_배수와_약수
     {
         static void Main(string[] args)
         {
-            _1978소수찾기.Solution();
+            _2581소수.Solution();
         }
     }
 }
