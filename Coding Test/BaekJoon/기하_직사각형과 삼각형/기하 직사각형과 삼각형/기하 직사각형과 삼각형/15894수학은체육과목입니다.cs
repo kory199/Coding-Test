0b8ecbb7f463f3351d1a52a16d68b9aa98af7713
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-06-13 11:43:37 +0900

    백준 15894 수학은 체육과목 입니다

---
+++ Coding Test/BaekJoon/기하_직사각형과 삼각형/기하 직사각형과 삼각형/기하 직사각형과 삼각형/15894수학은체육과목입니다.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Numerics;
+
+namespace 기하_직사각형과_삼각형
+{
+    class _15894수학은체육과목입니다
+    {
+        public void Solution()
+        {
+            BigInteger num = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(num * 4);
+        }
+    }
+}
