e3f8356593215517b3da1708bde1a72f7841cd43
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-06-13 14:33:47 +0900

    백준 10101 삼각형외우기2
    기존 삼각형 외우기와 다른 풀이
    -> Hashset 이용

--- Coding Test/BaekJoon/기하_직사각형과 삼각형/기하 직사각형과 삼각형/기하 직사각형과 삼각형/10101삼각형외우기2.cs
+++ Coding Test/BaekJoon/기하_직사각형과 삼각형/기하 직사각형과 삼각형/기하 직사각형과 삼각형/10101삼각형외우기2.cs
@@ -1,20 +1,46 @@
-﻿using System;
-using System.Linq;
+﻿using System.IO;
+using System.Text;
+using System;
+using System.Collections.Generic;
 
 namespace 기하_직사각형과_삼각형
 {
     class _10101삼각형외우기2
     {
+        static StreamReader sr = new StreamReader(Console.OpenStandardInput(), Encoding.Default);
+        static StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), Encoding.Default);
+
         public static void Solution()
         {
-            int[] angle = new int[3];
-            int count = 0;
-            int maxCount = 0;
+            // hashset 중복된 요소를 배제한 유일한 요소 저장
+            HashSet<int> set = new HashSet<int>();
+            int a = int.Parse(sr.ReadLine());
+            int b = int.Parse(sr.ReadLine());
+            int c = int.Parse(sr.ReadLine());
+            set.Add(a);
+            set.Add(b);
+            set.Add(c);
+            if ((a + b + c).Equals(180))
+            {
+                if (set.Count.Equals(1))
+                {
+                    sw.Write("Equilateral");
+                }
+                else if (set.Count.Equals(2))
+                {
+                    sw.Write("Isosceles");
+                }
+                else
+                {
+                    sw.Write("Scalene");
+                }
 
-            for (int i = 0; i < 3; i++)
-                angle[i] = int.Parse(Console.ReadLine());
-
-            
+            }
+            else
+            {
+                sw.Write("Error");
+            }
+            sw.Close();
         }
     }
 }
