acc8ca37a57feca605ff5bbb46923748c7a4ca6c
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-06-23 11:29:24 +0900

    구현 상하좌우
    다른 풀이 추가

--- Coding Test/4.구현_시뮬레이션과 완전 탐색/구현/구현/구현_상하좌우.cs
+++ Coding Test/4.구현_시뮬레이션과 완전 탐색/구현/구현/구현_상하좌우.cs
@@ -19,6 +19,7 @@ namespace 구현
     {
         public static void Solution()
         {
+            #region 내 풀이
             int mapSize = int.Parse(Console.ReadLine());
             string[] movePosition = Console.ReadLine().Split();
             int playerPositionX = 1;
@@ -56,6 +57,43 @@ namespace 구현
                 }
             }
             Console.WriteLine($"{playerPositionX} {playerPositionY}");
+            #endregion
+
+            #region 이코테 풀이
+            // N을 입력받기
+            int n = int.Parse(Console.ReadLine());
+            string[] plans = Console.ReadLine().Split(' ');
+            int x = 1, y = 1;
+
+            // L, R, U, D에 따른 이동 방향 
+            int[] dx = { 0, 0, -1, 1 };
+            int[] dy = { -1, 1, 0, 0 };
+            char[] moveTypes = { 'L', 'R', 'U', 'D' };
+
+            // 이동 계획을 하나씩 확인
+            for (int i = 0; i < plans.Length; i++)
+            {
+                char plan = plans[i][0];
+                // 이동 후 좌표 구하기 
+                int nx = -1, ny = -1;
+                for (int j = 0; j < 4; j++)
+                {
+                    if (plan == moveTypes[j])
+                    {
+                        nx = x + dx[j];
+                        ny = y + dy[j];
+                    }
+                }
+                // 공간을 벗어나는 경우 무시 
+                if (nx < 1 || ny < 1 || nx > n || ny > n)
+                    continue;
+                // 이동 수행 
+                x = nx;
+                y = ny;
+            }
+
+            Console.WriteLine(x + " " + y);
+            #endregion
         }
     }
 }
