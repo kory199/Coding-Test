24348e96a80eb0d5accaa8315d3abd0d22fd7bae
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-06-23 15:02:00 +0900

    재귀함수_팩토리얼

---
+++ Coding Test/5. 재귀함수/재귀함수/재귀함수/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 재귀함수
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            재귀함수_팩토리얼.Solution();
+        }
+    }
+}
---
+++ Coding Test/5. 재귀함수/재귀함수/재귀함수/재귀함수_팩토리얼.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 재귀함수
+{
+    class 재귀함수_팩토리얼
+    {
+       public static void Solution()
+        {
+            int num = 5;
+
+            Console.WriteLine(Factorial(num));
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            return n*Factorial(n - 1);
+        }
+    }
+}
