6a26a10c16cd0e6088f0e9d2af22737fdf28683c
kory199 <97496901+kory199@users.noreply.github.com>
2023-07-03 17:48:18 +0900

    백준 정렬
    나이순 정렬 : string builder 풀이 추가

--- Coding Test/1.BaekJoon/정렬/SortAlgorithm/SortAlgorithm/나이순 정렬.cs
+++ Coding Test/1.BaekJoon/정렬/SortAlgorithm/SortAlgorithm/나이순 정렬.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace SortAlgorithm
 {
@@ -24,6 +25,29 @@ namespace SortAlgorithm
 
             foreach (var item in answer)
                 Console.WriteLine(item.Item1 + " " + item.Item2);
+        }
+
+        public static void SolutionSB()
+        {
+            StringBuilder sb = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            List<(int, string)> playerInfo = new List<(int, string)>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputStr = Console.ReadLine().Split();
+                int age = int.Parse(inputStr[0]);
+                string name = inputStr[1];
+
+                playerInfo.Add((age, name));
+            }
+
+            var answer = playerInfo.OrderBy(x => x.Item1).ToList();
+
+            foreach (var item in answer)
+                sb.AppendLine(item.Item1 + " " + item.Item2);
+
+            Console.WriteLine(sb);
         }
     }
 }
