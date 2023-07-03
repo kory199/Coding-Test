b2e6fd11b2f1d7bd02460004762f799782e791f8
kory199 <97496901+kory199@users.noreply.github.com>
2023-07-03 11:04:08 +0900

    백준 정렬
    수 정렬하기(오름차순)

---
+++ Coding Test/1.BaekJoon/정렬/SortAlgorithm/SortAlgorithm/수 정렬하기.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithm
+{
+    internal class 수_정렬하기
+    {
+        public static void Solution()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] inputArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+                inputArray[i] = int.Parse(Console.ReadLine());
+
+            Array.Sort(inputArray);
+
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(inputArray[i]);
+        }
+    }
+}
