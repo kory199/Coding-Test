ccaacb7c74a8f4f7d7ee588a584cbff72c60eaf4
kory199 <97496901+kory199@users.noreply.github.com>
2023-07-06 13:42:57 +0900

    백준 스택
    스택

---
+++ Coding Test/1.BaekJoon/스택/BaekJoonStack/BaekJoonStack/10828_스택.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaekJoonStack
+{
+    internal class _10828_스택
+    {
+        public static void Solution()
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            Stack<string> testCase = new Stack<string>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                string[] temp = Console.ReadLine().Split();
+
+                switch (temp[0])
+                {
+                    case "push":
+                        testCase.Push(temp[1]);
+                        break;
+                    case "pop":
+                        sb.AppendLine(testCase.Count ==0 ? "-1" : testCase.Pop());
+                        break;
+                    case "size":
+                        sb.AppendLine(testCase.Count().ToString());
+                        break;
+                    case "empty":
+                        sb.AppendLine(testCase.Count == 0 ? "1" : "0");
+                        break;
+                    case "top":
+                        sb.AppendLine(testCase.Count == 0 ? "-1" : testCase.Peek());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ Coding Test/1.BaekJoon/스택/BaekJoonStack/BaekJoonStack/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoonStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _10828_스택.Solution();
+        }
+    }
+}
