1fd8a00595adaf75e3f04fc860587c52f6121914
kory199 <97496901+kory199@users.noreply.github.com>
2023-07-10 10:30:03 +0900

    백준 스택
    1874 스택 수열

--- Coding Test/1.BaekJoon/스택/BaekJoonStack/BaekJoonStack/1874 스택 수열.cs
+++ Coding Test/1.BaekJoon/스택/BaekJoonStack/BaekJoonStack/1874 스택 수열.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BaekJoonStack
 {
@@ -10,7 +8,40 @@ namespace BaekJoonStack
     {
         public static void Solution()
         {
+            int inputNum = int.Parse(Console.ReadLine());
+            Stack<int> testNum = new Stack<int>();
+            StringBuilder sb = new StringBuilder();
+            int maxNum = 1;
+            bool isCheck = true;
 
+            for (int i = 1; i <= inputNum; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+
+                if (!isCheck)
+                    break;
+
+                while (temp >= maxNum)
+                {
+                    testNum.Push(maxNum);
+                    sb.AppendLine("+");
+                    maxNum++;
+                }
+
+                if (testNum.Peek() != temp)
+                {
+                    isCheck = false;
+                    break;
+                }
+
+                testNum.Pop();
+                sb.AppendLine("-");
+            }
+
+            if (isCheck)
+                Console.WriteLine(sb);
+            else
+                Console.WriteLine("NO");
         }
     }
 }
